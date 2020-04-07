e5391fd2249b7129900753cf46738cd7c90e4460
avellarmedia <mktd@avellarmedia.com>
2020-04-07 15:26:19 -0300

    Make Logo Component Dynamic

--- src/components/atom/logo-cria/index.js
+++ src/components/atom/logo-cria/index.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react';
-import svg from './logo-cria.svg';
-import style from './style.module.css';
+import React, { Component } from 'react'
+
+import Cria from './logoParts/Cria'
+import TagLine from './logoParts/TagLine'
+import SchoolBottom from './logoParts/SchoolBottom'
+import SchoolSide from './logoParts/SchoolSide'
+
+import style from './style.module.css'
 
 export default class LogoCria extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            version : props.version || "mini"
+        }
+    }
     render() {
+        
+        const { state } = this
+        
+        const COMPLETA = (state.version === "completa") || false
+        const HORIZONTAL = (state.version === "horizontal") || false
+        const VERTICAL = (state.version === "vertical") || false
+
         return (
-            <img className={style.img} src={svg} alt='Logo Cria School | MindBlowing Education' title='Cria School' />
+            <figure className={`${style.figure} ${style[state.version]}`}>
+                <Cria className={style.cria}/> {(COMPLETA || HORIZONTAL) && <SchoolSide className={style.schoolSide}/>}
+                {(COMPLETA) && <TagLine className={style.tagLine}/>}
+                {(VERTICAL) && <SchoolBottom className={style.schoolBottom}/>}
+            </figure>
         )
     }
 }
