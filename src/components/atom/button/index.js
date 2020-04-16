fd18490a9722c24007afc069c1b8512fa1e07a41
avellarmedia <mktd@avellarmedia.com>
2020-04-16 17:27:58 -0300

    Fix Color Button Default

--- src/components/atom/button/index.js
+++ src/components/atom/button/index.js
@@ -4,8 +4,8 @@ import style from './style.module.css';
 
 export default class Button extends PureComponent {
     
-    buttonStyle = style[`${this.props.style}` || 'pink'] 
-    
+    buttonStyle = this.props.style ? style[this.props.style] : style.pink
+
     render(){
         return(
             <button disabled={this.props.disabled} onClick={this.props.onClick} type={this.props.type || "button"} className={`${style.button} ${this.buttonStyle} ${this.props.className}`}>
