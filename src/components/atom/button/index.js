46ef54755b56a79060d46c167edc2815d73db9f9
avellarmedia <mktd@avellarmedia.com>
2020-06-03 10:09:55 -0300

    Button Accepts Style Props

--- src/components/atom/button/index.js
+++ src/components/atom/button/index.js
@@ -7,8 +7,11 @@ export default class Button extends PureComponent {
     buttonStyle = this.props.colorSchema ? style[this.props.colorSchema] : style.pink
 
     render(){
+        const inlineStyle = {
+            ...this.props.style
+        }
         return(
-            <button disabled={this.props.disabled} onClick={this.props.onClick} type={this.props.type || "button"} className={`${style.button} ${this.buttonStyle} ${this.props.className}`}>
+            <button style={inlineStyle} disabled={this.props.disabled} onClick={this.props.onClick} type={this.props.type || "button"} className={`${style.button} ${this.buttonStyle} ${this.props.className}`}>
                 {this.props.children}
             </button>
         )
